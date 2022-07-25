971a47792b4f341dfa6c96cf04de601020164ffa
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-07-24 20:46:03 -0400

    Initial commit, set up screens/pages to fill in

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import TestComponent from './components/TestComponent';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your appp!</Text>
+      <TestComponent />
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/TestComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const TestComponent = () => {
+  return (
+    <Text>This is a test component</Text>
+  )
+}
+
+export default TestComponent
