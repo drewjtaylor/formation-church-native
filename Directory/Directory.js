0991d9d5df0922482ef293014c6678339dd977fc
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-07-29 11:37:55 -0400

    Made address conditional in directory

--- Directory/Directory.js
+++ Directory/Directory.js
@@ -1,16 +1,24 @@
 import users from "../demodata/demoUsers";
 import { Text, StyleSheet, FlatList } from "react-native";
 import { Card, Image } from "react-native-elements";
+import { useSelector } from 'react-redux';
 
 const Directory = () => {
 
+    // const users = useSelector((state) => state.users);
+
   const renderItem = ({ item }) => {
-    const {fname, lname, email, avatarImage, isMember} = item;
+    const {fname, lname, email, avatarImage, address1, address2, city, isMember} = item;
     return (
       <Card>
         <Card.Title>{`${fname} ${lname}`}</Card.Title>
         <Card.Divider />
         <Text>Email: {email}</Text>
+        {address1 ? <Text>Address:</Text> : null}
+        {address1 ? <Text>{address1}</Text> : null}
+        {address2 ? <Text>{address2}</Text> : null}
+        <Text>{city}</Text>
+        <Text>{fname} {isMember ? 'is' : 'is not'} a member{isMember? null : ' yet'}.</Text>
         <Image
           style={styles.avatarImage}
         //   source={ `../demodata/${avatarImage}` }
