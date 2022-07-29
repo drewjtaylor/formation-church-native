384d1ef0ddc1572ccf79ff108c89f740daae3be3
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-07-29 10:13:46 -0400

    prepped fetch/asyncthunk for users

--- Directory/directorySlice.js
+++ Directory/directorySlice.js
@@ -1,29 +1,50 @@
 import users from "../demodata/demoUsers";
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
+  const response = await fetch(baseUrl + "users");
+  return response.json();
+});
 
 const initialState = {
-    usersArray: users
+  usersArray: users,
 };
 
 const usersSlice = createSlice({
-    name: 'users',
-    initialState,
-    reducers: {
-        addUser: (state, action) => {
-            const newUser = {
-                id: state.usersArray.length+1,
-                ...action.payload
-            };
-            state.usersArray.push(newUser);
-        },
-        toggleIsMember: (state, action) => {
-            state.usersArray[action.payload].isMember = !(state.usersArray[action.payload].isMember)   //Payload should give the id number of the user
-        },
-        toggleIsAdmin: (state, action) => {
-            state.usersArray[action.payload].isAdmin = !(state.usersArray[action.payload].isAdmin)   //Payload should give the id number of the user
-        }
-    }
-})
+  name: "users",
+  initialState,
+  reducers: {
+    addUser: (state, action) => {
+      const newUser = {
+        id: state.usersArray.length + 1,
+        ...action.payload,
+      };
+      state.usersArray.push(newUser);
+    },
+    toggleIsMember: (state, action) => {
+      state.usersArray[action.payload].isMember =
+        !state.usersArray[action.payload].isMember; //Payload should give the id number of the user
+    },
+    toggleIsAdmin: (state, action) => {
+      state.usersArray[action.payload].isAdmin =
+        !state.usersArray[action.payload].isAdmin; //Payload should give the id number of the user
+    },
+  },
+  extraReducers: {
+    [fetchUsers.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [fetchUsers.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.errMess = null;
+      state.campsitesArray = action.payload;
+    },
+    [fetchUsers.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.errMess = action.error ? action.error.message : "Fetch failed";
+    },
+  },
+});
 
 export const usersReducer = usersSlice.reducer;
 
