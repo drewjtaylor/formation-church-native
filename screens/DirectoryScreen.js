b6f2f53faa8783ba422b540226ed3b98c25caa14
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-08-11 13:35:03 -0400

    "Add directory entry" email link working

--- screens/DirectoryScreen.js
+++ screens/DirectoryScreen.js
@@ -1,28 +1,43 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import Directory from '../Directory/Directory';
-import ImagePickerExample from '../components/ImagePicker';
+import React from "react";
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import Directory from "../Directory/Directory";
+import * as MailComposer from "expo-mail-composer";
 
 const DirectoryScreen = () => {
+  const emailDirectoryInfo = () => {
+    MailComposer.composeAsync({
+      recipients: ["admin@formationchurch.com"],
+      subject: "New directory information",
+      body: "This email was generated from the Formation Church app. Please add my information to the directory.",
+    });
+  };
+
   return (
     <>
-    <Text style={styles.heading}>Welcome to the Church Directory</Text>
-    <Text style={styles.subHeading}>If you'd like to add your information, tap here.</Text>
-    <Directory />
+      <View>
+        <Text style={styles.heading}>Welcome to the Church Directory</Text>
+        <Pressable onPress={emailDirectoryInfo}>
+          <Text style={styles.subHeading}>
+            If you'd like to add your information, or your entry needs
+            updated/missing information, please tap here to email us.
+          </Text>
+        </Pressable>
+      </View>
+      <Directory />
     </>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    heading: {
-        fontWeight: 'bold',
-        fontSize: 24,
-        textAlign: 'center'
-    },
-    subHeading: {
-        fontSize: 18,
-        textAlign: 'center'
-    }
-})
+  heading: {
+    fontWeight: "bold",
+    fontSize: 24,
+    textAlign: "center",
+  },
+  subHeading: {
+    fontSize: 18,
+    textAlign: "center",
+  },
+});
 
-export default DirectoryScreen
+export default DirectoryScreen;
