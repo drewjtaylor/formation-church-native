4a57ef8f0c12be1ed761f2841bf43ae61bec9354
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-08-11 15:31:41 -0400

    Main about screen navigator buttons

--- screens/AboutScreens/AboutHome.js
+++ screens/AboutScreens/AboutHome.js
@@ -1,15 +1,66 @@
-import { View, Text } from "react-native";
-import { Button } from "react-native-elements";
+import { View, Text, StyleSheet } from "react-native";
+import { Button, Icon } from "react-native-elements";
 
-const AboutHome = ({navigation}) => {
+const AboutHome = ({ navigation }) => {
   return (
-    <View>
-      <Text>AboutHome</Text>
-      <Button title="Press here to go to location page" onPress={() => navigation.navigate('LocationInformation')} />
-      <Button title="Press here to go to our statement of faith" onPress={() => navigation.navigate('WhatWeBelieve')} />
-      <Button title="Press here to see why we're named Formation Church" onPress={() => navigation.navigate('WhyFormationChurch')} />
+    <View style={styles.mainTextBox}>
+      <Text >
+        Formation Church is a group of believers in the St. Petersburg area
+        committed to being Christ-centered in what we do and community-focused
+        in how we do it.
+      </Text>
+      <Button
+        title="Location and meeting information"
+        onPress={() => navigation.navigate("LocationInformation")}
+        buttonStyle={styles.button}
+        icon={
+          <Icon
+            name="map-o"
+            type="font-awesome"
+            color="white"
+            iconStyle={{ marginRight: 10 }}
+          />
+        }
+      />
+      <Button
+        title="Statement of Faith "
+        onPress={() => navigation.navigate("WhatWeBelieve")}
+        buttonStyle={styles.button}
+        icon={
+          <Icon
+            name="scroll"
+            type="font-awesome-5"
+            color="white"
+            iconStyle={{ marginRight: 10 }}
+          />
+        }
+      />
+      <Button
+        title="Why Formation Church?"
+        onPress={() => navigation.navigate("WhyFormationChurch")}
+        buttonStyle={styles.button}
+        icon={
+          <Icon
+            name="head-question"
+            type="material-community"
+            color="white"
+            iconStyle={{ marginRight: 10 }}
+          />
+        }
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  button: {
+    margin: 10,
+  },
+  mainTextBox: {
+    margin: 20,
+    marginBottom: 50,
+    
+  }
+});
 
 export default AboutHome;
